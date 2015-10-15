8354bc8e0f6be7b87602806f8d7ce26ede92af14
Sam Higton <contact@samhigton.com>
2015-10-15 22:32:50 +0100

    initial commit - basic template layout for index a series pages

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+require "vendor/autoload.php";
+
+$app = new \Slim\Slim(array(
+    'view' => new \Slim\Views\Twig()
+));
+
+$app->get('/', function () use ($app) {
+   $app->view()->display('index.twig', array ());
+});
+
+$app->get('/series/:id', function ($seriesId) use ($app) {
+    $app->view()->display('series.twig', array ());
+});
+
+$app->run();
