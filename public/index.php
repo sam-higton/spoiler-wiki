870967caf4991fdceb9d6392eb176c7b4257cc1d
Sam Higton <contact@samhigton.com>
2015-10-29 15:19:47 +0000

    adding more api calls

--- public/index.php
+++ public/index.php
@@ -38,22 +38,55 @@ $app->post('/api/generate-canon', function () use ($app) {
     $name = $app->request->post('name');
     $description = $app->request->post('description');
     $artistId = $app->request->post('artist_id');
+    $workTypeId = $app->request->post('work_type_id');
 
     $canon = new \SpoilerWiki\Canon();
     $canon->setName($name);
     $canon->setDescription($description);
     $canon->setPrimaryArtistId($artistId);
+    $canon->setWorkTypeId($workTypeId);
     $canonId = $canon->save();
     echo "id: " . $canonId;
 });
 
-$app->post('/api-generate-work', function () use ($app) {
+$app->post('/api/generate-work', function () use ($app) {
     $name = $app->request->post('name');
     $description = $app->request->post('description');
     $order = $app->request->post('order');
     $artistId = $app->request->post('artist_id');
-    $canonId = $app->request->post('artistId');
-    $workTypeId = $app->request->post('work_type_id');
+    $canonId = $app->request->post('canon_id');
+
+
+    $work = new \SpoilerWiki\Work();
+    $work->setName($name);
+    $work->setDescription($description);
+    $work->setOrder($order);
+    $work->setPrimaryArtistId($artistId);
+    $work->setCanonId($canonId);
+
+    $workId = $work->save();
+    echo "id: " . $workId;
+});
+
+$app->post('/api/generate-work-type', function () use ($app) {
+    $name = $app->request->post('name');
+    $workLabel = $app->request->post('work_label');
+    $milestoneLabel = $app->request->post('milestone_label');
+
+    $workType = new \SpoilerWiki\WorkType();
+    $workType->setName($name);
+    $workType->setWorkLabel($workLabel);
+    $workType->setmilestoneLabel($milestoneLabel);
+    $workTypeId = $workType->save();
+    echo "id: " . $workTypeId;
+
+});
+
+$app->post('/api/generate-topic', function () use ($app) {
+
+});
+
+$app->post('/api/generate-milestone', function () use ($app) {
 
 });
 
